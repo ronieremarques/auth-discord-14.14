144edc4dbcdcc352256a6e8c196915738c8df026
Roniere Marques <ronieremarques55@gmail.com>
2024-01-16 08:44:52 -0300

    tokens/session

--- discord-bot/events/ready.js
+++ discord-bot/events/ready.js
@@ -1,4 +1,6 @@
 const client = require("../index")
+const Discord = require("discord.js")
 client.on("ready", () => {
     console.log(`Eu estou em execução em ${client.user.username}`)
+    client.user.setActivity(`www.empreendedo.com`, { type: Discord.ActivityType.Playing });
   })
